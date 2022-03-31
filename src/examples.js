653dc7ba33dffa4149c8a78538f1d225b85c876e
nikeedev <myftr12394@gmail.com>
2022-03-31 16:36:38 +0200

    Updates for GameEngine.js!

--- src/examples.js
+++ src/examples.js
@@ -8,8 +8,8 @@ import { Player } from './modules/Player.js';
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
-canvas.width = 1200;
-canvas.height = 565;
+canvas.width = window.innerWidth - 30;
+canvas.height = window.innerHeight - 20;
 const ScreenSize = new Size(canvas.width, canvas.height);
 
 const colors = ["#FFFFFF", "#C0C0C0", "#808080", "#000000", "#FF0000", "#800000", "#FFFF00", "#808000", "#00FF00", "#008000", "#00FFFF"]
@@ -21,20 +21,13 @@ var y = 38;
 var colorcount1 = 0;
 var colorcount2 = 0;
 
-
-
-
-
-
-var game = new Player(x, y, 10, ScreenSize.width, ScreenSize.height);
-
-game.update(ctx, ()=>{}, 0) 
-
-
-
-
+var ActivateUp = false;
+var ActivateDown = false;
 
 /*
+
+
+// Game and Words variable: Rainbow text and rainbox moving Rect. 
 
 var words = new Text("Hello", x, y);
 
@@ -59,29 +52,67 @@ game.update(ctx, () => {
         if (colorcount2 > colors.length) colorcount2 = 0;
     }
     else {
-        game.x += 0;
         game.color = "#ffffff"
     }
 }, false, 100);
 
 
 
+/*
+// Game2: Canvas painting of rainbow rectangles that move around.
 var game2 = new Rect(0, 0, 1, ScreenSize.width, ScreenSize.height)
 
 
 game2.update(ctx, () => {
 
-    game2.x += 1;
-    game2.y += 1.2;
 
-    game2.size += 0.1;
+    if (ActivateUp == false && canvas.height <= game2.y) {
+        ActivateUp = true;
+    }
+    if (ActivateDown == false && -2 >= game2.y) {
+        ActivateDown = true;
+        ActivateUp = false;
+    }
 
-    game2.color = colors[colorcount2];
-    colorcount2++;
-    if (colorcount2 > colors.length) colorcount2 = 0;
-}, false, 100);
+    if (ActivateUp) {
+        game2.x += 1;
+        game2.y -= 1;
+        game2.size -= 0.1;
+
+        game2.color = colors[colorcount2];
+        colorcount2--;
+        if (colorcount2 < 0) colorcount2 = colors.length;
+    }
+    else if (ActivateDown && game2.x >= 0) {
+    
+        game2.x -= 1;
+
+        
+
+        game2.color = colors[colorcount2];
+        colorcount2++;
+        if (colorcount2 > colors.length) colorcount2 = 0;
+
+    }
+    else {
+        game2.x += 1;
+        game2.y += 1;
+        game2.size += 0.1;
+
+        game2.color = colors[colorcount2];
+        colorcount2++;
+        if (colorcount2 > colors.length) colorcount2 = 0;
+    }
+    
 
 
+}, false);
+
+*/
+
+
+// Game3 variable: a Rect that moves after your mouse ( both x and y). If holding mouse down (any button) if will stop clearing the screen and leave the mark untill mouse buttons get released
+/*
 var game3 = new Rect(ScreenSize.width/2, ScreenSize.height/2, 20, ScreenSize.width, ScreenSize.height);
 
 
