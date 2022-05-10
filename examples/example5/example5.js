1920a3f99e50b301becc3c1e7f1f2cf89f8e4ebc
Nikita <69197950+nikeedev@users.noreply.github.com>
2022-05-10 10:49:31 +0200

    Update example5.js

--- examples/example5/example5.js
+++ examples/example5/example5.js
@@ -24,7 +24,7 @@ const ScreenSize = new GameEngine.Size(scr.CanvasSize.width, scr.CanvasSize.heig
 
 
 
-var game = new GameEngine.Graphics.Sprite("../../sprites/Black_Square.png", new GameEngine.Vector2(120, 120), ScreenSize);
+var game = new GameEngine.Graphics.Sprite("../../assets/Black_Square.png", new GameEngine.Vector2(120, 120), ScreenSize);
 
 
 await game.init();
